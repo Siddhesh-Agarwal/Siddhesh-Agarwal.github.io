11ffb09cb04d982fa2e3f6a8b2cb7f8b45dd0980
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-11-12 18:42:28 +0530

    small updated

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,10 +1,13 @@
 import {
     SiApachespark,
+    SiAmazonaws,
     SiBootstrap,
     SiC,
+    SiCloudflare,
     SiCplusplus,
     SiDjango,
     SiDocker,
+    SiExpress,
     SiFastapi,
     SiFlask,
     SiGit,
@@ -18,11 +21,12 @@ import {
     SiMongodb,
     SiMysql,
     SiNextdotjs,
+    SiNodedotjs,
     SiPhp,
     SiPostgresql,
     SiPowershell,
     SiPython,
-    SiPytorch,
+    SiReact,
     SiRedis,
     SiScikitlearn,
     SiScipy,
@@ -33,17 +37,14 @@ import {
     SiTwitter,
     SiTypescript,
     SiVercel,
-    SiExpress,
-    SiNodedotjs,
-    SiReact,
 } from "react-icons/si";
-import Typewriter from "typewriter-effect";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { SocialLink } from "@/types/SocialLink";
 import { ToolLink } from "@/types/ToolLink";
 import Image from "next/image";
 import profilePic from "@/public/me-according-to-chatgpt.webp"
+import { textSize } from "@/lib/utils";
 
 const links: ToolLink[] = [
     { name: "Python", icon: SiPython },
@@ -53,7 +54,6 @@ const links: ToolLink[] = [
     { name: "SciPy", icon: SiScipy },
     { name: "Scikit Learn", icon: SiScikitlearn },
     { name: "TensorFlow", icon: SiTensorflow },
-    { name: "PyTorch", icon: SiPytorch },
     { name: "Streamlit", icon: SiStreamlit },
     { name: "C", icon: SiC },
     { name: "C++", icon: SiCplusplus },
@@ -79,6 +79,8 @@ const links: ToolLink[] = [
     { name: "GNU Bash", icon: SiGnubash },
     { name: "Powershell", icon: SiPowershell },
     { name: "Docker", icon: SiDocker },
+    { name: "AWS", icon: SiAmazonaws },
+    { name: "Cloudflare", icon: SiCloudflare },
     { name: "Vercel", icon: SiVercel },
 ];
 
@@ -100,124 +102,29 @@ const socialLinks: SocialLink[] = [
     },
 ]
 
-function SocialLinks() {
-    return (
-        <div className="row-auto">
-            <div className="col-span-12 flex flex-wrap justify-center">
-                {
-                    socialLinks.map((link, index) => {
-                        return (
-                            <Link
-                                href={link.link}
-                                key={index}
-                                rel="noreferrer noopener" target="_blank"
-                                passHref
-                            >
-                                <motion.div
-                                    className="m-1 bg-blue-600 dark:bg-blue-500 rounded px-2 py-1 text-white text-md md:text-lg lg:text-xl w-fit cursor-pointer"
-                                    whileHover={{ scale: 1.1 }}
-                                    whileTap={{ scale: 0.9 }}
-                                >
-                                    <link.icon className="inline-block" />
-                                    <p className="inline-block mx-2">
-                                        {link.name}
-                                    </p>
-                                </motion.div>
-                            </Link>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
-
-
-function Tools() {
-    return (
-        <div className="inline-flex justify-center w-full md:max-w-xl lg:max-w-3xl flex-wrap px-4">
-            {
-                links.map(
-                    (link: ToolLink, index: number) => {
-                        return (
-                            <motion.div
-                                className="m-2 text-blue-600 dark:text-blue-500 font-semibold inline-block text-md md:text-lg lg:text-xl w-fit cursor-pointer"
-                                whileHover={{ scale: 1.25 }}
-                                whileTap={{ scale: 0.8 }}
-                                key={index}
-                                title={link.name}
-                            >
-                                <link.icon className="inline-block" />
-                            </motion.div>
-                        )
-                    }
-                )
-            }
-        </div>
-    )
-}
-
-
-function Roles() {
-    const DELAY = 1500;
-    return (
-        <motion.span
-            initial={{ opacity: 0 }}
-            animate={{ opacity: DELAY / 1000 }}
-            transition={{ delay: DELAY / 1000 }}
-            className="text-blue-600 dark:text-blue-500 inline-flex text-center justify-center"
-        >
-            <Typewriter
-                onInit={
-                    (typewriter) => {
-                        typewriter
-                            .typeString("Full Stack Developer")
-                            .pauseFor(DELAY)
-                            .deleteAll()
-                            .pauseFor(DELAY / 4)
-                            .typeString("ML Engineer")
-                            .pauseFor(DELAY)
-                            .deleteAll()
-                            .pauseFor(DELAY / 4)
-                            .start()
-                    }
-                }
-                options={{
-                    autoStart: true,
-                    loop: true,
-                    delay: 20,
-                    deleteSpeed: 10,
-                    cursor: "_",
-                }}
-            />
-        </motion.span>
-    )
-}
-
 
 export const Intro: React.FC = () => {
     return (
-        <div className="container py-4 dark:text-white max-w-4xl">
-            {/* <div className="my-5">
-                <div className="col-12 text-center">
-                    <h1 className="text-4xl md:text-5xl lg:text-7xl font-normal">Hello, I&apos;m <span className="text-blue-600 dark:text-blue-500 font-bolder">Siddhesh</span></h1>
-                    <h2 className="text-3xl md:text-4xl lg:text-5xl font-sans">
-                        <Roles />
-                    </h2>
+        <div className="container py-4 dark:text-white max-w-6xl">
+            <div className="my-5">
+                <div className="text-center">
+                    <h1 className={`${textSize("4xl")} font-normal inline`}>
+                        Hello, I&apos;m{' '}<span className="text-blue-600 dark:text-blue-500 font-bolder">Siddhesh</span>
+                    </h1>
                 </div>
-            </div> */}
+            </div>
 
             {/* Divide into 2 parts */}
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 <div className="my-5">
                     <p className="text-justify">
-                        Hello! I am <b>Siddhesh Agarwal</b>, a passionate software engineering student with a focus on <b>algorithm optimization, problem-solving, and code efficiency</b>. My journey is marked by <b>hands-on experience</b> with complex coding problems, where I enjoy tackling time complexity challenges and finding creative ways to maximize performance. As I apply for SDE internships, I am excited to bring my drive for continuous improvement, deep understanding of coding fundamentals, and passion for technology to real-world projects that make an impact.
+                        Hello! I&apos;m <b>Siddhesh Agarwal</b>, a <b>passionate software engineering student</b> with a focus on <b>developing efficient, scalable solutions</b>. I have been programming for the past 5 years, since I was a high school student. Currently, I&apos;m working on honing my skills in <b>algorithm optimization, problem-solving, and code efficiency</b>, especially in preparation for Software Development Engineer (SDE) roles. My journey is marked by <b>hands-on experience with complex coding problems</b>, where I enjoy tackling time complexity challenges and finding creative ways to maximize performance. As I apply for SDE internships, I am excited to bring my drive for <b>continuous improvement, deep understanding of coding fundamentals, and passion for technology</b> to real-world projects. I look forward to contributing to innovative solutions that make an impact.
                     </p>
                 </div>
                 <div className="my-5">
-                    <Image src={profilePic} alt="Siddhesh Agarwal" className="w-full"/>
+                    <Image src={profilePic} alt="Siddhesh Agarwal" className="w-full h-auto rounded-md shadow-lg" />
                     <div className="text-center text-muted-foreground">
-                        Me according to GPT-4o <code className="font-mono">UwU</code>
+                        Me according to GPT-4o and Dall-e
                     </div>
                 </div>
             </div>
@@ -225,18 +132,62 @@ export const Intro: React.FC = () => {
             {/* List things proficient in  */}
             <div className="my-5">
                 <div className="text-center">
-                    <h3 className="text-lg md:text-xl lg:text-2xl mb-3">
+                    <h3 className={`${textSize("2xl")} mb-3`}>
                         I&apos;m proficient in:
                     </h3>
-                    <Tools />
+                    <div className="inline-flex justify-center w-full md:max-w-xl lg:max-w-3xl flex-wrap px-4">
+                        {
+                            links.map(
+                                (link: ToolLink, index: number) => {
+                                    return (
+                                        <motion.div
+                                            className={`m-2 text-blue-600 dark:text-blue-500 font-semibold inline-block ${textSize("xl")} w-fit cursor-pointer`}
+                                            whileHover={{ scale: 1.25 }}
+                                            whileTap={{ scale: 0.8 }}
+                                            key={index}
+                                            title={link.name}
+                                        >
+                                            <link.icon className="inline-block" />
+                                        </motion.div>
+                                    )
+                                }
+                            )
+                        }
+                    </div>
                 </div>
             </div>
 
             {/* Ways to contact me */}
             <div className="my-5">
-                <div className="col-12 text-center">
-                    <h3 className="text-lg md:text-xl lg:text-2xl mb-3">You can contact me on:</h3>
-                    <SocialLinks />
+                <div className="text-center">
+                    <h3 className={`${textSize("2xl")} mb-3`}>You can contact me on:</h3>
+                    <div className="row-auto">
+                        <div className="col-span-12 flex flex-wrap justify-center">
+                            {
+                                socialLinks.map((link, index) => {
+                                    return (
+                                        <Link
+                                            href={link.link}
+                                            key={index}
+                                            rel="noreferrer noopener" target="_blank"
+                                            passHref
+                                        >
+                                            <motion.div
+                                                className="m-1 bg-blue-600 dark:bg-blue-500 rounded px-2 py-1 text-white text-md md:text-lg lg:text-xl w-fit cursor-pointer"
+                                                whileHover={{ scale: 1.1 }}
+                                                whileTap={{ scale: 0.9 }}
+                                            >
+                                                <link.icon className="inline-block" />
+                                                <p className="inline-block mx-2">
+                                                    {link.name}
+                                                </p>
+                                            </motion.div>
+                                        </Link>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function textSize(size: string) {
+  const sizes = ["xs", "sm", "md", "lg", "xl", "2xl", "3xl", "4xl", "5xl", "6xl", "7xl", "8xl", "9xl"]
+  const index = sizes.indexOf(size)
+  if( index === -1 ) {
+    throw "Invalid text size"
+  }
+  return `text-${size} md:text-${sizes[index + 1]} lg:text-${sizes[index + 2]}`
+}
