150a4c6b1b5fde0acb0d83a3a7536fc22d94cab1
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2025-09-15 08:43:01 +0530

    Removed unused function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,22 +1,12 @@
-import { CustomDate } from "@/types/Utils";
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import type { CustomDate } from "@/types/Utils";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
-
-export function textSize(size: string) {
-  const sizes = ["xs", "sm", "md", "lg", "xl", "2xl", "3xl", "4xl", "5xl", "6xl", "7xl", "8xl", "9xl"]
-  const index = sizes.indexOf(size)
-  if( index === -1 ) {
-    throw "Invalid text size"
-  }
-  return `text-${size} md:text-${sizes[index + 1]} lg:text-${sizes[index + 2]}`
+  return twMerge(clsx(inputs));
 }
 
 export function formatDate(date: CustomDate): string {
-  const month = (date.month < 10) ? `0${date.month}` : `${date.month}`
-  return `${month}/${date.year}`
+  const month = date.month < 10 ? `0${date.month}` : `${date.month}`;
+  return `${month}/${date.year}`;
 }
